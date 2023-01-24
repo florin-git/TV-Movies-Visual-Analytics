92167a9bfaec99ed5da3731a9c3ba64641bc70cb
Florin <flocuconasu10@gmail.com>
2023-01-24 12:25:11 +0100

    Changing Legend to MDS

--- vis/mds.js
+++ vis/mds.js
@@ -7,6 +7,10 @@ var sky = ["Sky Drama", "Sky Due", "Sky Suspense", "Sky Comedy", "Sky Action"];
 var mediaset = ["Italia 1", "Iris", "Rete 4", "Cine34"];
 var other = ["Cielo"];
 
+var colors = ["#f5f5f5", "#d8b365", "#5ab4ac"];
+var textLegend = ["Mediaset", "Sky", "Other"];
+var monthLegend = ["", ""];
+
 var margin = { top: 0, right: 0, bottom: 0, left: 5 };
 var width = 470 - margin.left - margin.right;
 var height = 340 - margin.top - margin.bottom;
@@ -19,12 +23,17 @@ var x = d3.scaleLinear().domain([-42, 58]).range([0, width]);
 var y = d3.scaleLinear().domain([-58, 38]).range([height, 0]);
 
 var filteredData;
+var moviesFirstColor = [];
+var moviesSecondColor = [];
+var moviesThirdColor = [];
 
 function startMDS(selected_info) {
   d3.csv(DATASET_PATH, function (data) {
     // If the array is empty, we are not filtering data
     if (selected_info == null) {
       createMDS(data);
+      textLegend = ["Mediaset", "Sky", "Other"];
+      monthLegend = ["", ""];
       createLegend();
       return;
     }
@@ -33,6 +42,11 @@ function startMDS(selected_info) {
 
     // Filtering from Stacked
     if (selected_info.name == "stacked") {
+      // Reset colors arrays
+      moviesFirstColor = [];
+      moviesSecondColor = [];
+      moviesThirdColor = [];
+
       /**
        * When the user clicks on a buble on the Stacked,
        * in MDS we will have all the points belonging to
@@ -43,16 +57,80 @@ function startMDS(selected_info) {
         if (
           mediaset.includes(selected_info.channel) &
           mediaset.includes(d.channel)
-        )
+        ) {
+          // The other channels in this network are colored with the third color
+          if (d.channel != selected_info.channel) {
+            moviesThirdColor.push(d.id);
+            textLegend[2] = "Mediaset";
+          }
+          // This channel
+          else {
+            // The movies of the clicked month are colored with the first color
+            if (d.month == selected_info.month) {
+              moviesFirstColor.push(d.id);
+              textLegend[0] = d.channel;
+              monthLegend[0] = "(" + d.month + ")";
+            }
+            // The movies of the others months are colored with the second color
+            else {
+              moviesSecondColor.push(d.id);
+              textLegend[1] = d.channel;
+              monthLegend[1] = "(others)";
+            }
+          }
+
           return d;
+        }
 
         // If in Sky
-        if (sky.includes(selected_info.channel) & sky.includes(d.channel))
+        if (sky.includes(selected_info.channel) & sky.includes(d.channel)) {
+          // The other channels in this network are colored with the third color
+          if (d.channel != selected_info.channel) {
+            moviesThirdColor.push(d.id);
+            textLegend[2] = "Sky";
+          }
+          // This channel
+          else {
+            // The movies of the clicked month are colored with the first color
+            if (d.month == selected_info.month) {
+              moviesFirstColor.push(d.id);
+              textLegend[0] = d.channel;
+              monthLegend[0] = "(" + d.month + ")";
+            }
+            // The movies of the others months are colored with the second color
+            else {
+              moviesSecondColor.push(d.id);
+              textLegend[1] = d.channel;
+              monthLegend[1] = "(others)";
+            }
+          }
+
           return d;
+        }
 
         // If in Other
-        if (other.includes(selected_info.channel) & other.includes(d.channel))
+        if (other.includes(selected_info.channel) & other.includes(d.channel)) {
+          // The other channels in this network are colored with the third color
+          if (d.channel != selected_info.channel) {
+            moviesThirdColor.push(d.id);
+            textLegend[2] = "Other";
+          }
+          // This channel
+          else {
+            // The movies of the clicked month are colored with the first color
+            if (d.month == selected_info.month) {
+              moviesFirstColor.push(d.id);
+              textLegend[0] = d.channel + "(" + d.month + ")";
+            }
+            // The movies of the others months are colored with the second color
+            else {
+              moviesSecondColor.push(d.id);
+              textLegend[1] = d.channel + "(others)";
+            }
+          }
+
           return d;
+        }
       });
 
       filteredData = chosenData;
@@ -183,19 +261,49 @@ function createMDS(chosenData) {
     .style("pointer-events", "all")
     .style("cursor", "pointer")
     .style("fill", function (d) {
-      if (sky.includes(d.channel)) {
+      // The following happens if you click on the Stacked
+      if (
+        (moviesFirstColor.length != 0) &
+        (moviesSecondColor.length != 0) &
+        (moviesThirdColor.length != 0)
+      ) {
+        // Color of the clicked channel and month
+        if (moviesFirstColor.includes(d.id)) {
+          return colors[0];
+        }
+        // Color of the clicked channel and other months
+        else if (moviesSecondColor.includes(d.id)) {
+          return colors[1];
+        }
+        // Color all others channels in network
+        else if (moviesThirdColor.includes(d.id)) {
+          return colors[2];
+        }
+      }
+
+      // At the beginning
+      if (mediaset.includes(d.channel)) {
         // return "#7fc97f";
-        return "#d8b365";
-      } else if (mediaset.includes(d.channel)) {
+        return colors[0];
+      } else if (sky.includes(d.channel)) {
         // return "#beaed4";
-        return "#f5f5f5";
+        return colors[1];
       } else {
         // return "#fdc086";
-        return "#5ab4ac";
+        return colors[2];
       }
     })
     .on("mouseover", function (d) {
-      tooltip.html(d.title);
+      tooltip.html(
+        d.title +
+          "<br>Genres: " +
+          d.genres +
+          "<br>channel: " +
+          d.channel +
+          "<br>Month: " +
+          d.month
+      );
+
       return tooltip.style("visibility", "visible");
     })
     .on("mousemove", function () {
@@ -294,45 +402,66 @@ function updateCalendar() {
 function createLegend() {
   var svg = d3.select("#area_mds").select("svg");
 
+  // Cirlces
   svg
     .append("circle")
     .attr("cx", width - 0.2 * width)
     .attr("cy", 80)
     .attr("r", 6)
-    .style("fill", "#7fc97f");
+    .style("fill", colors[0]);
   svg
     .append("circle")
     .attr("cx", width - 0.2 * width)
-    .attr("cy", 110)
+    .attr("cy", 115)
     .attr("r", 6)
-    .style("fill", "#beaed4");
+    .style("fill", colors[1]);
   svg
     .append("circle")
     .attr("cx", width - 0.2 * width)
-    .attr("cy", 140)
+    .attr("cy", 150)
     .attr("r", 6)
-    .style("fill", "#fdc086");
+    .style("fill", colors[2]);
+
+  // Text
   svg
     .append("text")
     .attr("x", width - 0.18 * width)
     .attr("y", 81)
-    .text("Sky")
+    .text(textLegend[0])
     .style("fill", "#fff")
     .style("font-size", "15px")
     .attr("alignment-baseline", "middle");
   svg
     .append("text")
     .attr("x", width - 0.18 * width)
-    .attr("y", 111)
-    .text("Mediaset")
+    .attr("y", 95)
+    .text(monthLegend[0])
+    .style("fill", "#fff")
+    .style("font-size", "15px")
+    .attr("alignment-baseline", "middle");
+
+  svg
+    .append("text")
+    .attr("x", width - 0.18 * width)
+    .attr("y", 115)
+    .text(textLegend[1])
     .style("fill", "#fff")
     .style("font-size", "15px")
     .attr("alignment-baseline", "middle");
   svg
     .append("text")
     .attr("x", width - 0.18 * width)
-    .attr("y", 141)
-    .text("Other")
+    .attr("y", 130)
+    .text(monthLegend[1])
+    .style("fill", "#fff")
+    .style("font-size", "15px")
+    .attr("alignment-baseline", "middle");
+
+  svg
+    .append("text")
+    .attr("x", width - 0.18 * width)
+    .attr("y", 150)
+    .text(textLegend[2])
     .style("fill", "#fff")
     .style("font-size", "15px")
     .attr("alignment-baseline", "middle");
